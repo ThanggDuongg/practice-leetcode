db90f9495e8457e7180746bffd38f00ef83143c4
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-09 21:37:52 +0700

    LTC: 20, 759

---
+++ Leetcode/20.cs
@@ -0,0 +1,34 @@
+﻿namespace Leetcode
+{
+    class _20
+    {
+        public bool IsValid(string s)
+        {
+            var mapping = new Dictionary<char, char>
+            {
+                { ')', '(' },
+                { '}', '{' },
+                { ']', '[' },
+            };
+
+            var stack = new Stack<char>();
+
+            foreach (var c in s)
+            {
+                if (!mapping.ContainsKey(c))
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    if (stack.Count == 0 || stack.Pop() != mapping[c])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ Leetcode/759.cs
@@ -0,0 +1,39 @@
+﻿namespace Leetcode
+{
+    class _759
+    {
+        public int[][] EmployeeFreeTime(int[][][] schedule)
+        {
+            var intervals = new List<int[]>();
+            foreach (var employee in schedule)
+            {
+                foreach (var item in employee)
+                {
+                    intervals.Add(item);
+                }
+            }
+
+            Array.Sort(intervals.ToArray(), (x, y) => x[0].CompareTo(y[0]));
+            var merged = new List<int[]>();
+
+            foreach (var interval in intervals)
+            {
+                if (interval.Length <= 0 || interval[0] >= merged[^1][1])
+                {
+                    merged.Add(interval);
+                }
+                else
+                {
+                    merged[^1][1] = Math.Max(merged[^1][1], interval[1]);
+                }
+            }
+
+            var res = new List<int[]>();
+            for (int i = 1; i < merged.Count; i++)
+            {
+                res.Add([merged[i - 1][1], merged[i][0]]);
+            }
+            return [.. res];
+        }
+    }
+}
