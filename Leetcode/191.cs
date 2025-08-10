be5512c53e5a2b2e35c2bc0ef8fcfd04043e8ed2
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-10 17:12:29 +0700

    LTC: 191, 338

---
+++ Leetcode/191.cs
@@ -0,0 +1,18 @@
+﻿namespace Leetcode
+{
+    internal class _191
+    {
+        public int HammingWeight(int n)
+        {
+            int res = 0;
+
+            while (n != 0)
+            {
+                res++;
+                n &= (n - 1);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Leetcode/338.cs
@@ -0,0 +1,16 @@
+﻿namespace Leetcode
+{
+    internal class _338
+    {
+        public int[] CountBits(int n)
+        {
+            var res = new int[n + 1];
+            res[0] = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                res[i] = res[i >> 1] + (i & 1);
+            }
+            return res;
+        }
+    }
+}
