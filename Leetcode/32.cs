0cb23fe2fe164a5fe0388dfad8644db1866b2c21
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-13 22:39:37 +0700

    LTC: 32

---
+++ Leetcode/32.cs
@@ -0,0 +1,34 @@
+﻿namespace Leetcode
+{
+    internal class _32
+    {
+        public int LongestValidParentheses(string s)
+        {
+            var res = 0;
+            var stack = new Stack<int>(-1);
+            stack.Push(-1);
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else
+                {
+                    stack.Pop();
+                    if (stack.Count > 0)
+                    {
+                        res = Math.Max(res, i - stack.Peek());
+                    }
+                    else
+                    {
+                        stack.Push(i);
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
