91ef7c6602de23ce67b316522a700cf48c5f0726
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-13 21:59:55 +0700

    LTC: 326

---
+++ Leetcode/326.cs
@@ -0,0 +1,20 @@
+﻿namespace Leetcode
+{
+    internal class _326
+    {
+        public bool IsPowerOfThree(int n)
+        {
+            if (n <= 0)
+            {
+                return false;
+            }
+
+            while (n % 3 == 0)
+            {
+                n /= 3;
+            }
+
+            return n == 1;
+        }
+    }
+}
