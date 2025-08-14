4aa6e29c82e8643241130a01368c178020f848c5
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-14 22:03:20 +0700

    LTC: 2264, 739

---
+++ Leetcode/2264.cs
@@ -0,0 +1,36 @@
+﻿namespace Leetcode
+{
+    internal class _2264
+    {
+        public string LargestGoodInteger(string num)
+        {
+            int i = 0,
+                currentMax = -1;
+
+            while (i < num.Length)
+            {
+                if (i + 2 > num.Length - 1)
+                {
+                    break;
+                }
+
+                var isValid = num[i] == num[i + 1] && num[i] == num[i + 2];
+                if (isValid)
+                {
+                    i += 2;
+                    var newValue = num[i] - '0';
+                    if (newValue > currentMax)
+                    {
+                        currentMax = newValue;
+                    }
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return currentMax != -1 ? $"{currentMax}{currentMax}{currentMax}" : string.Empty;
+        }
+    }
+}
---
+++ Leetcode/739.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode
+{
+    internal class _739
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            var res = new int[temperatures.Length];
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < temperatures.Length; i++)
+            {
+                while (stack.Count > 0 && temperatures[i] > temperatures[stack.Peek()])
+                {
+                    var idx = stack.Pop();
+                    res[idx] = i - idx;
+                }
+                stack.Push(i);
+            }
+
+            return res;
+        }
+    }
+}
