ad1d33d5a10c1b05fdc0d6c367b198ba59dad58c
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-15 21:48:53 +0700

    LTC: 342

---
+++ Leetcode/342.cs
@@ -0,0 +1,15 @@
+﻿namespace Leetcode
+{
+    internal class _342
+    {
+        public bool IsPowerOfFour(int n)
+        {
+            if (n == 0)
+            {
+                return false;
+            }
+
+            return (n & n - 1) == 0 && (n - 1) % 3 == 0;
+        }
+    }
+}
