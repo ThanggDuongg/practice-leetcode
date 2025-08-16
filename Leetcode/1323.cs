599b89ba10eb78b48e6b086a93d2665da88a8fc6
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-16 16:00:18 +0700

    LTC: 1323

---
+++ Leetcode/1323.cs
@@ -0,0 +1,18 @@
+﻿namespace Leetcode
+{
+    internal class _1323
+    {
+        public int Maximum69Number(int num)
+        {
+            var str = num.ToString();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '6')
+                {
+                    return int.Parse(string.Concat(str.AsSpan(0, i), "9", str.AsSpan(i + 1)));
+                }
+            }
+            return num;
+        }
+    }
+}
