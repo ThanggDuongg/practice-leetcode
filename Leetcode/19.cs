23f8095bc47a490f7dab23d5d5600282a18e4315
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-18 22:44:21 +0700

    LTC: 19

---
+++ Leetcode/19.cs
@@ -0,0 +1,26 @@
+﻿namespace Leetcode
+{
+    internal class _19
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var dummy = new ListNode(-1) { next = head };
+
+            ListNode fast = dummy,
+                slow = dummy;
+            for (int i = 0; i <= n; i++)
+            {
+                fast = fast.next;
+            }
+
+            while (fast != null)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+
+            slow.next = slow.next.next;
+            return dummy.next;
+        }
+    }
+}
