8378eae8da67d66f2da59b1c9fe5ce5d96502cff
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-19 21:49:49 +0700

    LTC: 2348

---
+++ Leetcode/2348.cs
@@ -0,0 +1,30 @@
+﻿namespace Leetcode
+{
+    internal class _2348
+    {
+        public long ZeroFilledSubarray(int[] nums)
+        {
+            int zerorCount = 0;
+            long res = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    zerorCount++;
+                }
+                else
+                {
+                    res += (long)zerorCount * (zerorCount + 1) / 2;
+                    zerorCount = 0;
+                }
+            }
+
+            if (zerorCount != 0)
+            {
+                res += (long)zerorCount * (zerorCount + 1) / 2;
+            }
+            return res;
+        }
+    }
+}
