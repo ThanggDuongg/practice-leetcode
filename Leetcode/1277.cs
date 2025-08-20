a2772627337631a33ccb30ada6e2bbad69d9392e
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-20 22:57:17 +0700

    LTC: 1277

---
+++ Leetcode/1277.cs
@@ -0,0 +1,28 @@
+﻿namespace Leetcode
+{
+    internal class _1277
+    {
+        public int CountSquares(int[][] matrix)
+        {
+            var res = 0;
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[0].Length; j++)
+                {
+                    if (matrix[i][j] == 1 && i > 0 && j > 0)
+                    {
+                        matrix[i][j] =
+                            Math.Min(
+                                Math.Min(matrix[i - 1][j], matrix[i][j - 1]),
+                                matrix[i - 1][j - 1]
+                            ) + 1;
+                    }
+                    res += matrix[i][j];
+                }
+            }
+
+            return res;
+        }
+    }
+}
