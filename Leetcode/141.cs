e111ed9eb11ae622b53a195ad86a8b3f98183862
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-21 22:36:54 +0700

    LTC: 141

---
+++ Leetcode/141.cs
@@ -0,0 +1,25 @@
+﻿namespace Leetcode
+{
+    internal class _141
+    {
+        public bool HasCycle(ListNode head)
+        {
+            if (head is null || head.next is null)
+            {
+                return false;
+            }
+
+            var visisted = new HashSet<ListNode>();
+            while (head != null)
+            {
+                if (visisted.Contains(head))
+                {
+                    return true;
+                }
+                visisted.Add(head);
+                head = head.next;
+            }
+            return false;
+        }
+    }
+}
