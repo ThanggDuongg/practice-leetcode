cf07ec60badf8c8cbb5f47e7f83499c3ce7a32fd
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-24 11:05:58 +0700

    LTC: 1493

---
+++ Leetcode/1493.cs
@@ -0,0 +1,37 @@
+﻿namespace Leetcode
+{
+    internal class _1493
+    {
+        public int LongestSubarray(int[] nums)
+        {
+            int res = 0,
+                deletedCount = 0,
+                left = 0,
+                right = 0;
+
+            while (right < nums.Length)
+            {
+                if (nums[right] == 0)
+                {
+                    if (deletedCount == 0)
+                    {
+                        deletedCount++;
+                    }
+                    else
+                    {
+                        while (nums[left] != 0)
+                        {
+                            left++;
+                        }
+                        left++;
+                    }
+                }
+
+                res = Math.Max(res, right - left + 1);
+                right++;
+            }
+
+            return res;
+        }
+    }
+}
