94e9f987f1513e7dd655ce477ea135db427ea77f
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-26 21:47:59 +0700

    LTC: 498, 3000

---
+++ Leetcode/3000.cs
@@ -0,0 +1,27 @@
+﻿namespace Leetcode
+{
+  internal class _3000
+  {
+    public int AreaOfMaxDiagonal(int[][] dimensions)
+    {
+      int res = 0,
+        maxDiagonal = 0;
+
+      foreach (var dim in dimensions)
+      {
+        int diagonal = dim[0] * dim[0] + dim[1] * dim[1];
+        if (diagonal > maxDiagonal)
+        {
+          maxDiagonal = diagonal;
+          res = dim[0] * dim[1];
+        }
+        else if (diagonal == maxDiagonal)
+        {
+          res = Math.Max(res, dim[0] * dim[1]);
+        }
+      }
+
+      return res;
+    }
+  }
+}
---
+++ Leetcode/498.cs
@@ -0,0 +1,60 @@
+﻿namespace Leetcode
+{
+  internal class _498
+  {
+    public int[] FindDiagonalOrder(int[][] mat)
+    {
+      int m = mat.Length,
+        n = mat[0].Length,
+        totalLen = m * n;
+      var res = new int[totalLen];
+
+      int curX = 0,
+        curY = 0,
+        i = 0;
+      var isUp = true;
+      while (i < totalLen)
+      {
+        res[i++] = mat[curX][curY];
+        if (!isUp)
+        {
+          if (curX == m - 1)
+          {
+            curY++;
+            isUp = true;
+          }
+          else if (curY == 0)
+          {
+            curX++;
+            isUp = true;
+          }
+          else
+          {
+            curY--;
+            curX++;
+          }
+        }
+        else
+        {
+          if (curY == n - 1)
+          {
+            curX++;
+            isUp = false;
+          }
+          else if (curX == 0)
+          {
+            curY++;
+            isUp = false;
+          }
+          else
+          {
+            curY++;
+            curX--;
+          }
+        }
+      }
+
+      return res;
+    }
+  }
+}
