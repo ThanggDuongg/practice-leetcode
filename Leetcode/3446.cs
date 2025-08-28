488abfa607b0f244f34890539345d2188adea2c6
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-28 23:44:10 +0700

    LTC: 3446

---
+++ Leetcode/3446.cs
@@ -0,0 +1,52 @@
+﻿namespace Leetcode
+{
+  internal class _3446
+  {
+    public int[][] SortMatrix(int[][] grid)
+    {
+      var len = grid.Length;
+      var start = len - 2;
+
+      while (start >= 0)
+      {
+        var list = new List<int>();
+        int i = start,
+          j = 0;
+        while (i < len)
+        {
+          list.Add(grid[i++][j++]);
+        }
+        list.Sort((x, y) => y.CompareTo(x));
+        i = start;
+        j = 0;
+        while (i < len)
+        {
+          grid[i++][j] = list[j++];
+        }
+        start--;
+      }
+
+      start = 1;
+      while (start < len)
+      {
+        var list = new List<int>();
+        int i = 0,
+          j = start;
+        while (j < len)
+        {
+          list.Add(grid[i++][j++]);
+        }
+        list.Sort((x, y) => x.CompareTo(y));
+        i = 0;
+        j = start;
+        while (j < len)
+        {
+          grid[i][j++] = list[i++];
+        }
+        start++;
+      }
+
+      return grid;
+    }
+  }
+}
