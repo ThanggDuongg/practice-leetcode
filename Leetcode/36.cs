6cbb33aad5dc4b028494a631e067a8c044d641b2
ThanggDuongg <thangduc.duong14@gmail.com>
2025-08-31 15:09:50 +0700

    LTC: 36, 37

---
+++ Leetcode/36.cs
@@ -0,0 +1,35 @@
+﻿namespace Leetcode
+{
+  internal class _36
+  {
+    public bool IsValidSudoku(char[][] board)
+    {
+      var rows = new bool[9, 9];
+      var cols = new bool[9, 9];
+      var boxes = new bool[9, 9];
+
+      for (int i = 0; i < 9; i++)
+      {
+        for (int j = 0; j < 9; j++)
+        {
+          if (board[i][j] == '.')
+          {
+            continue;
+          }
+
+          var num = board[i][j] - '1';
+          var boxIndex = (i / 3) * 3 + j / 3;
+          if (rows[i, num] || cols[j, num] || boxes[boxIndex, num])
+          {
+            return false;
+          }
+          rows[i, num] = true;
+          cols[j, num] = true;
+          boxes[boxIndex, num] = true;
+        }
+      }
+
+      return true;
+    }
+  }
+}
---
+++ Leetcode/37.cs
@@ -0,0 +1,59 @@
+﻿namespace Leetcode
+{
+  internal class _37
+  {
+    public void SolveSudoku(char[][] board)
+    {
+      Backtrack(board);
+    }
+
+    private bool Backtrack(char[][] board)
+    {
+      for (var i = 0; i < 9; i++)
+      {
+        for (var j = 0; j < 9; j++)
+        {
+          if (board[i][j] == '.')
+          {
+            for (var c = '1'; c <= '9'; c++)
+            {
+              if (IsValid(i, j, board, c))
+              {
+                board[i][j] = c;
+                if (Backtrack(board))
+                {
+                  return true;
+                }
+                board[i][j] = '.';
+              }
+            }
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+
+    private bool IsValid(int x, int y, char[][] board, char c)
+    {
+      for (int i = 0; i < 9; i++)
+      {
+        if (board[i][y] == c)
+        {
+          return false;
+        }
+
+        if (board[x][i] == c)
+        {
+          return false;
+        }
+
+        if (board[(x / 3) * 3 + i / 3][(y / 3) * 3 + i % 3] == c)
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+}
