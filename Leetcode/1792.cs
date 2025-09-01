743d3220e50e18ead8306027f7c8d763acd2e111
ThanggDuongg <thangduc.duong14@gmail.com>
2025-09-01 10:52:26 +0700

    LTC: 1792

---
+++ Leetcode/1792.cs
@@ -0,0 +1,40 @@
+﻿namespace Leetcode
+{
+  internal class _1792
+  {
+    public double MaxAverageRatio(int[][] classes, int extraStudents)
+    {
+      var priority = new PriorityQueue<(int pass, int total), double>(
+        Comparer<double>.Create((x, y) => y.CompareTo(x))
+      );
+
+      var res = 0d;
+      foreach (var cls in classes)
+      {
+        var pass = cls[0];
+        var total = cls[1];
+        var currentRatio = (double)pass / total;
+        var newRatio = (double)(pass + 1) / (total + 1);
+        var diff = newRatio - currentRatio;
+        priority.Enqueue((pass, total), diff);
+        res += (double)pass / total;
+      }
+
+      while (extraStudents > 0)
+      {
+        var (pass, total) = priority.Dequeue();
+        res -= (double)pass / total;
+        pass++;
+        total++;
+        var currentRatio = (double)pass / total;
+        var newRatio = (double)(pass + 1) / (total + 1);
+        res += currentRatio;
+        var diff = newRatio - currentRatio;
+        priority.Enqueue((pass, total), diff);
+        extraStudents--;
+      }
+
+      return res / classes.Length;
+    }
+  }
+}
