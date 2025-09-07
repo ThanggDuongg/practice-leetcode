e2146f909b4cb68a17b9249dd117b87fa2522f3c
ThanggDuongg <thangduc.duong14@gmail.com>
2025-09-07 16:19:27 +0700

    LTC: 1304

---
+++ Leetcode/1304.cs
@@ -0,0 +1,25 @@
+﻿namespace Leetcode
+{
+    internal class _1304
+    {
+        public int[] SumZero(int n)
+        {
+            var res = new int[n];
+            int i = 0;
+
+            if (n % 2 != 0)
+            {
+                res[i++] = 0;
+            }
+
+            while (i < n)
+            {
+                var temp = i + 1;
+                res[i++] = temp;
+                res[i++] = -temp;
+            }
+
+            return res;
+        }
+    }
+}
