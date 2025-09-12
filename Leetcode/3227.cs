ac70ed9c1f259ffbe44de8b532f3760fd724497e
ThanggDuongg <thangduc.duong14@gmail.com>
2025-09-12 22:36:22 +0700

    LTC: 3227

---
+++ Leetcode/3227.cs
@@ -0,0 +1,18 @@
+﻿namespace Leetcode
+{
+    internal class _3227
+    {
+        public bool DoesAliceWin(string s)
+        {
+            var vowels = "aeiou";
+            foreach (var item in s)
+            {
+                if (vowels.Contains(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Leetcode/go/3227.go
@@ -0,0 +1,9 @@
+func doesAliceWin(s string) bool {
+    for i := 0; i < len(s); i++ {
+        if  s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' {
+            return true
+        }
+    }
+
+    return false
+}
