306b1d74e2543046a322b271280e3a28a4d4b2fa
ThanggDuongg <thangduc.duong14@gmail.com>
2025-09-23 22:13:12 +0700

    LTC: 165

---
+++ Leetcode/165.cs
@@ -0,0 +1,30 @@
+﻿namespace Leetcode
+{
+    internal class _165
+    {
+        public int CompareVersion(string version1, string version2)
+        {
+            var v1 = version1.Split('.').Select(int.Parse).ToArray();
+            var v2 = version2.Split('.').Select(int.Parse).ToArray();
+
+            int n = Math.Max(v1.Length, v2.Length),
+                i = 0;
+            while (i < n)
+            {
+                var v1i = i < v1.Length ? v1[i] : 0;
+                var v2i = i < v2.Length ? v2[i] : 0;
+
+                if (v1i > v2i)
+                {
+                    return 1;
+                }
+                else if (v1i < v2i)
+                {
+                    return -1;
+                }
+                i++;
+            }
+            return 0;
+        }
+    }
+}
